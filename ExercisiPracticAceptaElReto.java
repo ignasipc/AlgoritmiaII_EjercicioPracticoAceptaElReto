e48b12a23a19d816ebadd85103ae52c0ede4ab30
Ignasi Paredes Casasnovas <ignasipc22@gmail.com>
2025-03-25 13:13:48 +0100

    Creation of the git repository.

---
+++ ExercisiPracticAceptaElReto.java
@@ -0,0 +1,15 @@
+/**
+ * Fecha inicio:
+ * Fecha fin:
+ *
+ * @author Ignasi Paredes
+ */
+
+package exercisipracticaceptaelreto;
+
+public class ExercisiPracticAceptaElReto {
+    public static void main(String[] args) {
+        
+    }
+
+}
