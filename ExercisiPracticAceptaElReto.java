1406c0475769c9b5bdd217b043a94aa207f7d4c7
Ignasi Paredes Casasnovas <ignasipc22@gmail.com>
2025-03-25 19:02:01 +0100

    First test passed

--- ExercisiPracticAceptaElReto.java
+++ ExercisiPracticAceptaElReto.java
@@ -7,6 +7,8 @@
 
 package exercisipracticaceptaelreto;
 
+import java.util.Collections;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class ExercisiPracticAceptaElReto {
@@ -20,26 +22,46 @@ public class ExercisiPracticAceptaElReto {
         //En cada iteración, leeremos la primera línea, que contiene la cantidad 
         //de hangares disponibles en una determinada base, mientras no sea 0.
         for (int numeroHangares = scanner.nextInt(); numeroHangares != 0; numeroHangares = scanner.nextInt()) {
+            
+            //Obtenemos los H números con sus tamaños dentro de un array
+            String [] partesCapacidadHangares = new Scanner(System.in).nextLine().split(" ");
+            
             int [] hangares = new int [numeroHangares];
-            
-            scanner = new Scanner(System.in);
-            String lineaCapacidadHangares = scanner.nextLine();
-            String [] partesCapacidadHangares = lineaCapacidadHangares.split(" ");
-            
             for (int i = 0; i < numeroHangares; i++) {
                 hangares[i] = Integer.parseInt(partesCapacidadHangares[i]);
-//                System.out.print(hangares[i]+" ");
             }
-//            System.out.println("");
+
+            //Obtenemos la cantidad de naves que llegan a la base
+            int cantidadNaves = new Scanner(System.in).nextInt();
             
-            //scanner = new Scanner(System.in);
+            //Obtenemos los tamaños de las naves por orden de llegada
+            String [] partesCantidadNaves = new Scanner(System.in).nextLine().split(" ");
+            
+            int [] tamañoNaves = new int [cantidadNaves];
+            for (int i = 0; i < cantidadNaves; i++) {
+                tamañoNaves[i] = Integer.parseInt(partesCantidadNaves[i]);
+            }
+            
+            ////////////////////////////////////////////////////////////////////
+            // Ahora empezamos con el algoritmo heapSort
+            PriorityQueue<Integer> heap = new PriorityQueue<>(Collections.reverseOrder());
+            
+            for (int num : hangares) { 
+                heap.offer(num); 
+            }
+
+            String resultadoIteracion = "SI\n";
+            //Iteramos en cada nave por orden de llegada comprobando si podemos meterla
+            for (int nave : tamañoNaves) {
+                if (heap.peek() < nave) {
+                    resultadoIteracion = "NO\n";
+                    break;
+                }
+                heap.offer(heap.poll() - nave);
+            }
+            salida += resultadoIteracion;
         }
         
-//        String numeros [] = numeroHangares.split(" ");
-//        
-//        int [] hangares = new int [numeros.length];
-//        los H numeros con sus tamaños,
-//        la cantidad de naves que llegan a la base y seguido de N números con sus tamaños, en este órden.
+        System.out.print(salida);
     }
-
 }
