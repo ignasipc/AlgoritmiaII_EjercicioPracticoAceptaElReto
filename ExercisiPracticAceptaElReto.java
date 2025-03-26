2580749d7c8e931f9d6fbf0737e4b77586877cdb
Ignasi Paredes Casasnovas <ignasipc22@gmail.com>
2025-03-26 14:24:55 +0100

    Final version, changed comments and some minor changes

--- ExercisiPracticAceptaElReto.java
+++ ExercisiPracticAceptaElReto.java
@@ -15,13 +15,17 @@ public class ExercisiPracticAceptaElReto {
         //En cada iteración, leeremos la primera línea, que contiene la cantidad 
         //de hangares disponibles en una determinada base, mientras no sea 0.
         for (int numeroHangares = scanner.nextInt(); numeroHangares != 0; numeroHangares = scanner.nextInt()) {
+            //Creamos la cola de prioridad de hangares de tamaño H (1 ≤ H ≤ 10)
+            PriorityQueue<Hangar> heap = new PriorityQueue<>(numeroHangares);
             
-            PriorityQueue<Hangar> heap = new PriorityQueue<Hangar>();
-            
-            //Obtenemos los H números con sus tamaños dentro de un array
+            //Obtenemos los H números que indican la capacidad de los hangares
+            //y los vamos introduciendo en la cola de prioridad
+            //El bucle for se ejecuta n veces -> O(n)
             for (int i = 0; i < numeroHangares; i++) {
+                //El coste de offer() -> O(log(n))
                 heap.offer(new Hangar(scanner.nextInt()));
             }
+            //Coste total del algoritmo = O(n)*O(log(n)) -> O(n * log(n))
 
             //Obtenemos la cantidad de naves que llegan a la base
             int cantidadNaves = scanner.nextInt();
@@ -32,30 +36,47 @@ public class ExercisiPracticAceptaElReto {
                 tamañoNaves[i] = scanner.nextInt();
             }
 
-            String resultadoIteracion = "SI";
-            //Iteramos en cada nave por orden de llegada comprobando si podemos meterla
+            //Al inicio hay espacio suficiente para todas las naves
+            String espacioSuficiente = "SI";
+            //Iteramos en cada nave por orden de llegada comprobando si podemos 
+            //meter la nave en el hangar más grande.
+            //El bucle for se ejecuta n veces -> O(n)
             for (int nave : tamañoNaves) {
+                //Si no cabe la nave en el hangar más grande, quiere decir que no
+                //hay espacio suficiente, por tanto, marcamos que no hay espacio 
+                //suficiente y terminamos el bucle
+                //El coste de peek() -> O(1)
                 if (heap.peek().capacidad < nave) {
-                    resultadoIteracion = "NO";
+                    espacioSuficiente = "NO";
                     break;
                 }
+                
+                //Por el contrario, si hay espacio suficiente, le restamos ese espacio
+                //al hangar y lo volvemos a meter con su nuevo espacio disponible
+                //en la cola de prioridad
+                //El coste de poll() -> O(log(n))
                 heap.offer(new Hangar(heap.poll().capacidad - nave));
             }
-            System.out.println(resultadoIteracion);
+            //Coste total del algoritmo = O(n)*O(log(n)) -> O(n * log(n))
+            
+            //Imprimimos el resultado
+            System.out.println(espacioSuficiente);
         }
     }
     
     // Clase Hangar con prioridad según la capacidad
     public static class Hangar implements Comparable {
+        //Atributo indicando la capacidad del hangar
         private int capacidad;
 
+        //Método constructor
         Hangar(int capacidad) {
             this.capacidad = capacidad;
         }
 
+        //Modificamos el método compareTo para ordenar la capacidad de manera descendente
         @Override
         public int compareTo(Object o) {
-            //Ordenar de manera descendiente por capacidad
             return ((Hangar)o).capacidad - this.capacidad;
         }
     }
