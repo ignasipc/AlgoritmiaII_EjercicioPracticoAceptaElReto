a74545c33e5937ab8fdb5ccbd96a646d7d48889e
Ignasi Paredes Casasnovas <ignasipc22@gmail.com>
2025-03-27 11:16:22 +0100

    Final version. Deleted an array and got accepted in 'Acepta el reto'

--- ExercisiPracticAceptaElReto.java
+++ ExercisiPracticAceptaElReto.java
@@ -30,26 +30,23 @@ public class ExercisiPracticAceptaElReto {
             //Obtenemos la cantidad de naves que llegan a la base
             int cantidadNaves = scanner.nextInt();
             
-            //Obtenemos los tamaños de las naves por orden de llegada            
-            int [] tamañoNaves = new int [cantidadNaves];
-            for (int i = 0; i < cantidadNaves; i++) {
-                tamañoNaves[i] = scanner.nextInt();
-            }
-
             //Al inicio hay espacio suficiente para todas las naves
-            String espacioSuficiente = "SI";
+            boolean espacioSuficiente = true;
+            
             //Iteramos en cada nave por orden de llegada comprobando si podemos 
             //meter la nave en el hangar más grande.
             //El bucle for se ejecuta n veces -> O(n)
-            for (int nave : tamañoNaves) {
+            for (int i = 0; i < cantidadNaves; i++) {
+                //Obtenemos el tamaño de la nave
+                int nave = scanner.nextInt();
+                
+                if (!espacioSuficiente) continue;
+                
                 //Si no cabe la nave en el hangar más grande, quiere decir que no
                 //hay espacio suficiente, por tanto, marcamos que no hay espacio 
                 //suficiente y terminamos el bucle
                 //El coste de peek() -> O(1)
-                if (heap.peek().capacidad < nave) {
-                    espacioSuficiente = "NO";
-                    break;
-                }
+                if (heap.peek().capacidad < nave) espacioSuficiente = false;
                 
                 //Por el contrario, si hay espacio suficiente, le restamos ese espacio
                 //al hangar y lo volvemos a meter con su nuevo espacio disponible
@@ -60,7 +57,7 @@ public class ExercisiPracticAceptaElReto {
             //Coste total del algoritmo = O(n)*O(log(n)) -> O(n * log(n))
             
             //Imprimimos el resultado
-            System.out.println(espacioSuficiente);
+            System.out.println(espacioSuficiente ? "SI" : "NO");
         }
     }
     
